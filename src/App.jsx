172db8667aed8da469b958cf2906492aefefe930
YashJoshi1534 <joshi1532001@gmail.com>
2026-03-07 23:23:49 +0530

    solve

--- src/App.jsx
+++ src/App.jsx
@@ -33,9 +33,6 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage openPopup={openPopup} />} />
         <Route path="/pricing" element={<Pricing />} />
-
-        {/* This will automatically redirect things like /index.html or /random-text back to the home page */}
-        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
       <ContactPopup isOpen={popupOpen} onClose={() => setPopupOpen(false)} />
     </>
