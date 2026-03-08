f576f954383136359c8ffbb30056c1dd7e962ed4
YashJoshi1534 <joshi1532001@gmail.com>
2026-03-08 17:22:45 +0530

    solve

--- server.js
+++ server.js
@@ -1,16 +1,3 @@
-let app;
-try {
-    const module = await import('./server/index.js');
-    app = module.default;
-} catch (error) {
-    app = (req, res) => {
-        res.status(500).json({
-            success: false,
-            error: 'Vercel Initialization Crash!',
-            message: error.message,
-            stack: error.stack
-        });
-    };
-}
+import app from './server/index.js';
 
 export default app;
